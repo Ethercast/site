3f202005e18379410510ec30d6afa4785d105a3a
Ellie Day <heyellieday@gmail.com>
2018-02-18 00:27:54 -0700

    form updates

--- src/components/subscriptions/new.js
+++ src/components/subscriptions/new.js
@@ -2,6 +2,7 @@ import Button from 'grommet/components/Button';
 import Footer from 'grommet/components/Footer';
 import Form from 'grommet/components/Form';
 import FormField from 'grommet/components/FormField';
+import FormFields from 'grommet/components/FormFields';
 import Heading from 'grommet/components/Heading';
 import Box from 'grommet/components/Box';
 import AddIcon from 'grommet/components/icons/base/Add';
@@ -20,41 +21,50 @@ let NewSubscription = props => {
       }
       return (
         <div>
+          <Box colorIndex="light-2">
           {fields.map((condition, index) => {
             const inputStyle = {
-              width: '35%'
+              marginBottom: '0px',
             };
             return (<div key={index}>
-              <h4>
-                {index > 0 ? 'OR' : null}
-              </h4>
-              <FormField
-                label="Type"
-                style={inputStyle}
-              >
-                <Field
-                  name={`${condition}.type`}
-                  type="text"
-                  component="input"
-                />
-              </FormField>
-              <FormField
-                label="Value"
-                style={inputStyle}
-              >
-                <Field
-                  name={`${condition}.value`}
-                  type="text"
-                  component="input"
-                />
-              </FormField>
+                {index > 0 ? <h4>OR</h4> : null}
+                <FormFields>
+                  <Box direction="row">
+                    <FormField
+                      label="Type"
+                      style={inputStyle}
+                    >
+                      <Field
+                        name={`${condition}.type`}
+                        type="text"
+                        component="input"
+                      />
+                    </FormField>
+                    <FormField
+                      label="Value"
+                      style={inputStyle}
+                    >
+                      <Field
+                        name={`${condition}.value`}
+                        type="text"
+                        component="input"
+                      />
+                    </FormField>
+                  </Box>
+                </FormFields>
               {index === fields.length - 1 ? <Button
                 icon={<AddIcon/>}
                 onClick={() => fields.push({})}
                 primary={false}
               /> : null}
+              { fields.length > 0 ? <Button
+                icon={<CloseIcon/>}
+                onClick={() => fields.remove(index)}
+                primary={false}
+              /> : null } 
             </div>);
           })}
+          </Box>
         </div>);
     };
     return renderConditionFields;
@@ -62,37 +72,30 @@ let NewSubscription = props => {
 
   const renderLogicFields = ({ fields, meta: { error, submitFailed } }) => (
     <div>
+      <Heading tag="h3">
+        {fields.length > 0 ? 'Filters' : null}
+      </Heading>
       {fields.map((logic, index) => {
         return (
           <div>
             <Heading tag="h3">
               {index > 0 ? 'AND' : null}
-              <Button
-                onClick={() => fields.push({})}
-                primary={false}
-                label='Add Filter'
-              />
             </Heading>
-            <div style={{
-              border: '1px solid gray',
+            <Box style={{
+              border: '1px solid rgba(0,0,0,.15)',
               borderRadius: '2px',
               position: 'relative'
             }} key={index}>
-              <Button
-                icon={<CloseIcon/>}
-                onClick={() => fields.remove(index)}
-                primary={false}
-                style={{
-                  position: 'absolute',
-                  right: '5px',
-                  top: '10px'
-                }}
-              />
               <FieldArray name={`${logic}.conditions`}
                           component={makeRenderConditionFields(index)}/>
-            </div>
+            </Box>
           </div>);
       })}
+      <Button
+        onClick={() => fields.push({})}
+        primary={false}
+        label='Add Filter'
+      />
     </div>
   );
 
@@ -100,7 +103,7 @@ let NewSubscription = props => {
     <Box flex={true} align='center' direction='column' responsive={true} pad='small'>
     <Form style={style} onSubmit={handleSubmit}>
       <div>
-        {submitSucceeded ? <Toast status='ok'>Subscription successfully created. </Toast> : null}
+        {submitSucceeded ? <Toast status='ok'>Subscription successfully created.</Toast> : null}
         <Heading tag="h2">
           Create Subscription
         </Heading>
