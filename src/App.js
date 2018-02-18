286970f1c8399b56f8bb5a601423e5539bd01fbc
Salem <salmahmo@amazon.com>
2018-02-18 00:48:11 -0700

    fix the router bug

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 import { App as GrommetApp, Box, Button, Header, Title } from 'grommet';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, withRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import SubscriptionsIndex from './pages/subscriptions';
 import SubscriptionsNew from './pages/subscriptions/new';
 import Auth from './util/auth';
 
-export default connect(
-  ({ auth: { loggedIn } }) => ({ loggedIn }),
-  {
-    logout: () => {
-      return {
-        type: 'LOGGED_OUT'
-      };
+export default withRouter(
+  connect(
+    ({ auth: { loggedIn } }) => ({ loggedIn }),
+    {
+      logout: () => {
+        return {
+          type: 'LOGGED_OUT'
+        };
+      }
     }
-  }
-)(
-  class App extends Component {
-    render() {
-      const { loggedIn, logout } = this.props;
+  )(
+    class App extends Component {
+      render() {
+        const { loggedIn, logout } = this.props;
 
-      return (
-        <GrommetApp>
-          <Header>
-            <Title>
-              <Link to="/">if-eth</Link>
-            </Title>
+        return (
+          <GrommetApp>
+            <Header>
+              <Title>
+                <Link to="/">if-eth</Link>
+              </Title>
 
-            <Box flex={true} justify='end' direction='row' responsive={false}>
-              {
-                !loggedIn ?
-                  <Button label="Log in" onClick={Auth.login}/> :
-                  <Button label="Log out" onClick={logout}/>
-              }
-            </Box>
-          </Header>
+              <Box flex={true} justify='end' direction='row' responsive={false}>
+                {
+                  !loggedIn ?
+                    <Button label="Log in" onClick={Auth.login}/> :
+                    <Button label="Log out" onClick={logout}/>
+                }
+              </Box>
+            </Header>
 
-          <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/subscriptions/new" exact component={SubscriptionsNew}/>
-            <Route path="/subscriptions" exact component={SubscriptionsIndex}/>
-          </Switch>
-        </GrommetApp>
-      );
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/subscriptions/new" exact component={SubscriptionsNew}/>
+              <Route path="/subscriptions" exact component={SubscriptionsIndex}/>
+            </Switch>
+          </GrommetApp>
+        );
+      }
     }
-  }
+  )
 );
