b8eff5f2154054e8e16a0b7bfafe75f5af13ee7d
Salem <salmahmo@amazon.com>
2018-02-19 14:45:22 -0700

    just commit a fake home page to iterate on

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import Container from 'semantic-ui-react/dist/commonjs/elements/Container/Container';
-import Header from 'semantic-ui-react/dist/commonjs/elements/Header/Header';
 import PageRouter from './PageRouter';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Container>
-          <Header as="h3">
-            <Link to="/">if-eth</Link>
-          </Header>
-        </Container>
-
         <PageRouter/>
-
-        <Container>
-          Say <a href="mailto:hello@if-eth.com">hello@if-eth.com</a>&emsp;© 2018 if-eth
-        </Container>
       </div>
     );
   }
--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -3,12 +3,13 @@ import CreateSubscription from './pages/CreateSubscription';
 import ListSubscriptions from './pages/ListSubscriptions';
 import ViewSubscriptionPage from './pages/ViewSubscriptionPage';
 import NotFound from './pages/NotFound';
-import { Redirect, Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
+import HomepageLayout from './pages/HomePage';
 
 export default function PageRouter(props: {}) {
   return (
     <Switch>
-      <Route path="/" exact component={() => <Redirect to="/subscriptions"/>}/>
+      <Route path="/" exact component={HomepageLayout}/>
       <Route path="/subscriptions/new" exact component={CreateSubscription}/>
       <Route path="/subscriptions" exact component={ListSubscriptions}/>
       <Route path="/subscriptions/:id" exact component={ViewSubscriptionPage}/>
