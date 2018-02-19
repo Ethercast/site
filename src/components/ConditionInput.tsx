51e378010d89ecfc1101ae3180ec2afe851cc85d
Salem <salmahmo@amazon.com>
2018-02-19 10:20:59 -0700

    commit work to recursive form

--- src/components/ConditionInput.tsx
+++ src/components/ConditionInput.tsx
@@ -18,12 +18,16 @@ export default class ConditionInput extends React.Component<ConditionInputProps>
     return (
       <FormFields>
         <FormField label="Type">
-          <select onChange={e => onChange({ ...value, type: e.target.value as ConditionType })}>
+          <select
+            onChange={e => onChange({ ...value, type: e.target.value as ConditionType })}
+            value={value.type}
+            required
+          >
             {
               Object.keys(ConditionType)
                 .map(
                   (conditionType) => (
-                    <option selected={value.type === conditionType} key={conditionType} value={conditionType}>
+                    <option key={conditionType} value={conditionType}>
                       {CONDITION_NAMES[conditionType]}
                     </option>
                   )
@@ -31,13 +35,14 @@ export default class ConditionInput extends React.Component<ConditionInputProps>
             }
           </select>
         </FormField>
-        <FormField label={CONDITION_NAMES[value.type || ConditionType.address]}>
+        <FormField label={`${CONDITION_NAMES[value.type || ConditionType.address]} value`}>
           <input
             type="text"
             onChange={e => onChange({ ...value, value: e.target.value })}
             placeholder={value.type === ConditionType.address ? ZERO_ADDRESS : ZERO_TOPIC}
             pattern={`0x[a-fA-F0-9]{${value.type === ConditionType.address ? 40 : 64}}`}
-            required/>
+            required
+          />
         </FormField>
       </FormFields>
     );
--- src/components/LogicInput.tsx
+++ src/components/LogicInput.tsx
@@ -21,10 +21,50 @@ const TAG_STYLE = {
   display: 'inline-block'
 };
 
+// replace an item in an array
+function replaceItem<T>(item: T, items?: T[], index?: number): T[] {
+  const copy: T[] = items ? items.slice() : [];
+
+  if (!index) {
+    copy.push(item);
+  } else {
+    copy[index] = item;
+  }
+
+  return copy;
+}
+
 export function Separator({ style, ...props }: HTMLProps<HTMLDivElement>) {
   return (
     <div style={{ textAlign: 'center' }}>
       <div style={{ ...TAG_STYLE, ...style }} {...props}/>
+    </div>
+  );
+}
+
+function ConditionInputArray({ conditions, onChange }: { conditions: Partial<Condition>[], onChange: (conditions: Partial<Condition>[]) => void }) {
+  return (
+    <div>
+      {
+        conditions.map(
+          (condition, ix) => (
+            <div key={ix}>
+              <ConditionInput
+                value={condition}
+                onChange={changed => {
+                  const copy = conditions.slice();
+                  copy[ix] = changed;
+                  onChange(copy);
+                }}
+              />
+              <Button label="Remove condition" onClick={() => onChange(_.without(conditions, condition))}/>
+              {
+                ix < conditions.length - 1 ? <Separator>OR</Separator> : null
+              }
+            </div>
+          )
+        )
+      }
     </div>
   );
 }
@@ -38,26 +78,27 @@ export default class LogicInput extends React.Component<LogicInputProps> {
         {
           _.map(
             value || [],
-            (orConditions, andIx) => (
-              <div key={andIx}>
-                {
-                  _.map(
-                    orConditions,
-                    (condition, orIx) => (
-                      <ConditionInput value={condition} key={orIx} onChange={condition => null}/>
-                    )
-                  )
-                }
+            (orConditions, ix) => (
+              <div key={ix}>
+                <ConditionInputArray
+                  conditions={orConditions}
+                  onChange={conditions => {
+                    if (conditions.length > 0) {
+                      onChange(replaceItem<Partial<Condition>[]>(conditions, value, ix));
+                    } else {
+                      onChange(_.without((value || []), orConditions));
+                    }
+                  }}
+                />
                 <Button
                   label="Add 'OR' condition"
                   onClick={() => {
-                    onChange(
-                      // copy the top array
-                      (value || []).slice()
-                      // replace the and array with a new array that has a new element
-                        .splice(andIx, 1, [{}])
-                    );
+                    onChange(replaceItem<Partial<Condition>[]>([{ type: ConditionType.address }], value));
                   }}/>
+
+                {
+                  ix < (value || []).length - 1 ? <Separator>AND</Separator> : null
+                }
               </div>
             )
           )
