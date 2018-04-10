64710f82c3b86da6b1994df1049c06d13747aa67
Zach Pomerantz <zmp@umich.edu>
2018-04-10 16:33:11 -0400

    hide secret until revealed

--- src/components/api-keys/ApiKeyItem.tsx
+++ src/components/api-keys/ApiKeyItem.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Card, Input } from 'semantic-ui-react';
+import { Button, Card, Checkbox, Input, Label } from 'semantic-ui-react';
 import { ApiKey, Scope } from '../../debt/ethercast-backend-model';
 
 function ScopesList(props: {scopes: Scope[]}) {
@@ -17,33 +17,50 @@ interface Props {
   deleteApiKey: (id: string) => void;
 }
 
-export default ({ apiKey, deleteApiKey }: Props) => {
-  const { id, name, scopes, secret } = apiKey;
+export default class ApiKeyItem extends React.Component<Props, {revealSecret: boolean}> {
+  state = { revealSecret: false };
 
-  const copy = (e: any) => {
-    e.target.previousElementSibling.select();
-    document.execCommand('Copy');
-  };
+  render() {
+    const { id, name, scopes, secret } = this.props.apiKey;
+    const { revealSecret } = this.state;
 
-  return (
-    <Card fluid>
-      <Card.Content>
-        <Card.Header style={{ display: 'flex' }}>
-          <div style={{ flexGrow: 1 }}>{name}</div>
-          <div style={{ flexShrink: 0 }}>
-            <Button negative onClick={() => deleteApiKey(id)}>Delete</Button>
-          </div>
-        </Card.Header>
-        <Card.Description>
-          <ScopesList scopes={scopes}/>
-        </Card.Description>
+    const copy = (e: any) => {
+      e.target.previousElementSibling.select();
+      document.execCommand('Copy');
+    };
+
+    return (
+      <Card fluid>
         <Card.Content>
-          <Input fluid readOnly defaultValue={id}
-            action={{ color: 'teal', labelPosition: 'right', icon: 'copy', content: 'Copy ID', onClick: copy, style: { width: '12em' } }}/>
-          <Input fluid readOnly defaultValue={secret}
-            action={{ color: 'pink', labelPosition: 'right', icon: 'copy', content: 'Copy Secret', onClick: copy, style: { width: '12em' } }}/>
+          <Card.Header style={{ display: 'flex' }}>
+            <div style={{ flexGrow: 1 }}>{name}</div>
+            <div style={{ flexShrink: 0 }}>
+              <Button negative onClick={() => this.props.deleteApiKey(id)}>Delete</Button>
+            </div>
+          </Card.Header>
+          <Card.Description>
+            <ScopesList scopes={scopes}/>
+          </Card.Description>
+          <Card.Content>
+            <Input fluid action readOnly labelPosition='right' style={{ marginBottom: '1em' }}
+              defaultValue={id}>
+              <Label content="ID" style={{ width: '5em' }}/>
+              <input/>
+              <Button color='teal' labelPosition='right' icon='copy'
+                content='Copy ID' onClick={copy} style={{ width: '12em' }}/>
+            </Input>
+            <Input fluid action readOnly labelPosition='right' style={{ marginBottom: '1em' }}
+              defaultValue={secret} type={revealSecret ? 'text' : 'password'}>
+              <Label content='Secret' style={{ width: '5em' }}/>
+              <input/>
+              <Button disabled={!revealSecret} color='pink' labelPosition='right' icon='copy'
+                content='Copy Secret' onClick={copy} style={{ width: '12em' }}/>
+            </Input>
+            <Checkbox toggle checked={revealSecret} label='Reveal Secret'
+              onChange={(e: any, {checked: revealSecret}: any) => this.setState({revealSecret})}/>
+          </Card.Content>
         </Card.Content>
-      </Card.Content>
-    </Card>
-  );
-};
+      </Card>
+    );
+  }
+}
