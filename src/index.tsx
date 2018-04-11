e4d00267eacee8b4be1efa9d0251bd83c6e4599f
Moody Salem <salmahmo@amazon.com>
2018-04-10 21:14:26 -0400

    move the netinfo check to a better place

--- src/index.tsx
+++ src/index.tsx
@@ -7,29 +7,35 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Auth from './util/auth-util';
 import makeStore from './util/makeStore';
+import { netInfo } from './util/net-info';
 
-const store = makeStore();
+if (!netInfo || !netInfo.enabled) {
+  alert('sorry, this network is not yet supported!');
+  window.location.href = 'https://ethercast.io';
+} else {
+  const store = makeStore();
 
-store.dispatch(async dispatch => {
-  console.log('dispatched');
+  store.dispatch(async dispatch => {
+    console.log('dispatched');
 
-  dispatch({ type: 'AUTH_LOADING' });
+    dispatch({ type: 'AUTH_LOADING' });
 
-  try {
-    const result = await Auth.getUser();
-    console.log('logged in', result);
-    dispatch({ type: 'LOGGED_IN', payload: result });
-  } catch (error) {
-    console.error('failed to auth', error);
-    dispatch({ type: 'LOGGED_OUT' });
-  }
-});
+    try {
+      const result = await Auth.getUser();
+      console.log('logged in', result);
+      dispatch({ type: 'LOGGED_IN', payload: result });
+    } catch (error) {
+      console.error('failed to auth', error);
+      dispatch({ type: 'LOGGED_OUT' });
+    }
+  });
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+  );
+}
