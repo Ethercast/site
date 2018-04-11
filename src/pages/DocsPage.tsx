c01f693c10c60c43ca8d3bbb831817481ce5d772
Zach Pomerantz <zmp@umich.edu>
2018-04-11 02:06:04 -0400

    add routes, badges, links

--- src/pages/DocsPage.tsx
+++ src/pages/DocsPage.tsx
@@ -1,27 +1,131 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Container, Divider, Header } from 'semantic-ui-react';
+import { Container, Divider, Grid, Header, Icon, Label } from 'semantic-ui-react';
+
+enum HTTP {
+  POST = 'POST',
+  GET = 'GET',
+  DELETE = 'DELETE',
+}
+
+const createBadges = (repo: string) => {
+  const docsHref = `https://docs.ethercast.io/${repo}/`;
+  const repoHref = `https://github.com/Ethercast/${repo}`;
+
+  return (
+    <span style={{ marginLeft: '2rem' }}>
+      <Label as='a' href={docsHref} color='blue'>
+        <Icon name='book'/>
+        Docs
+      </Label>
+      <Label as='a' href={repoHref} color='grey'>
+        <Icon name='github'/>
+        Source
+      </Label>
+    </span>
+  );
+}
+
+const createBackendLink = (expects: string) => {
+  const href = `https://docs.ethercast.io/backend-model/interfaces/${expects.toLowerCase()}.html`;
+
+  return (
+    <a href={href}>{expects}</a>
+  );
+};
+
+const createRoute = (verb: HTTP, path: string, expects?: string) => {
+  return (
+    <li>
+    <Grid columns={3}>
+    <Grid.Column style={{ width: '5rem' }}>
+    <code><b>{verb}</b></code>
+    </Grid.Column>
+    <Grid.Column style={{ width: '10rem' }}>
+    <code>{path}</code>
+    </Grid.Column>
+    <Grid.Column>
+    {expects && createBackendLink(expects)}
+    </Grid.Column>
+    </Grid>
+    </li>
+  );
+};
 
 export default function Docs(props: RouteComponentProps<{}>) {
   return (
     <Container>
-      <Header as="h2">Documentation</Header>
+      <Header as='h2'>Documentation {createBadges('')}</Header>
       <p>
+        Ethercast lets you set up webhook subscriptions to events or transactions on the Ethereum Blockchain. Subscriptions can be set up throught the website, or using an API Key / Secret pair obtained through the website. This documentation covers how to interact with webhooks once you receive them, and how to subscribe to them through the API. If you want to create a subscription through the website, you can <a href='/subscriptions/new'>do that here</a>.
+      </p>
+      <p>
+        Documentation is been generated from typescript sources to help you use, parse, and validate webhooks and the API. Click on the docs / source button in any section to see definitions and source code.
       </p>
       <Divider/>
-      <Header as="h3"><a href="https://docs.ethercast.io/ethercast-model/">Webhook Documentation</a></Header>
+      <Header as='h3'>Webhooks</Header>
+      <Header as='h4'>Parsing {createBadges('ethercast-model')}</Header>
       <p>
+        Once you've made a subscription, you'll need to parse the incoming webhooks. Depending on the type of event you've subscribed to, you will either use the <a href='https://docs.ethercast.io/ethercast-model/interfaces/decodedlog.html'>DecodedLog</a> or <a href='https://docs.ethercast.io/ethercast-model/interfaces/decodedtransaction.html'>DecodedTransaction</a> interface. Note that the <code>ethercast</code> property of both contain the decoded ABI of the subscribed type.
       </p>
       <p>
-        Documentation for the webhook payload can be found at <a href="https://docs.ethercast.io/ethercast-model/">https://docs.ethercast.io/ethercast-model</a>.
+        If you subscribed through the website, you should also have been able to see an example of your payload.
+      </p>
+      <Header as='h4'>Validation {createBadges('calculate-signature')}</Header>
+      <p>
+        All payloads are signed with a secret particular to each subscription. Payloads can be validated using this signature with the <a href='https://docs.ethercast.io/calculate-signature/globals.html#isvalidsignature'>isValidSignature</a> function.
       </p>
       <Divider/>
-      <Header as="h3"><a href="https://docs.ethercast.io/backend-model/">API Documentation</a></Header>
+      <Header as='h3'>API Keys {createBadges('backend-model')}</Header>
       <p>
+        You can do anything that can be done using the website through an API instead, using an API key. If you don't yet have an API key, you can <a href='/api-keys/new'>create one here</a>. API keys and especially their secrets should not be shared - these are most appropriate for server-to-server commumnication. The keys permission scopes should  also be restricted to only what you need to avoid accidental misuse.
       </p>
       <p>
-        Documentation for the API can be found at <a href="https://docs.ethercast.io/backend-model/">https://docs.ethercast.io/backend-model</a>.
+        In order to send a request using the API, include your API key and secret in the request as the content of the <code>X-API-Key</code> and <code>X-API-Secret</code> headers, respectively.
       </p>
+      <Header as='h4'>Available Routes</Header>
+      <ul>
+        <li>
+          <Grid columns={3}>
+            <Grid.Column style={{ width: '5rem' }}>
+              <code><b>VERB</b></code>
+            </Grid.Column>
+            <Grid.Column style={{ width: '10rem' }}>
+              <code>path</code>
+            </Grid.Column>
+            <Grid.Column>
+              Expected body
+            </Grid.Column>
+          </Grid>
+        </li>
+        {createRoute(HTTP.POST, 'api-keys', 'CreateApiKeyRequest')}
+        {createRoute(HTTP.GET, 'api-keys')}
+        {createRoute(HTTP.DELETE, 'api-keys/{id}')}
+        {createRoute(HTTP.POST, 'subscriptions', 'CreateSubscriptionRequest')}
+        {createRoute(HTTP.GET, 'subscriptions')}
+        {createRoute(HTTP.GET, 'subscriptions/{id}')}
+        {createRoute(HTTP.GET, 'subscriptions/{id}/receipts')}
+        {createRoute(HTTP.DELETE, 'subscriptions/{id}')}
+        <li>
+          <Grid columns={3}>
+            <Grid.Column style={{ width: '5rem' }}>
+              <code><b>{HTTP.POST}</b></code>
+            </Grid.Column>
+            <Grid.Column style={{ width: '10rem' }}>
+              <code>get-examples</code>
+            </Grid.Column>
+            <Grid.Column>
+              {'{'}
+                type: <a href='https://docs.ethercast.io/backend-model/enums/subscriptiontype.html'>'SubscriptionType'</a>,
+                filters: 
+                  <a href='https://docs.ethercast.io/backend-model/globals.html#logsubscriptionfilters'>LogSubscriptionFilters</a>
+                  |
+                  <a href='https://docs.ethercast.io/backend-model/globals.html#transactionsubscriptionfilters'>TransactionSubscriptionFilters</a>
+              {'}'}
+            </Grid.Column>
+          </Grid>
+        </li>
+      </ul>
     </Container>
   );
 }
